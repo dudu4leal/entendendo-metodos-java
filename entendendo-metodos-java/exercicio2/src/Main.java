f39f72c2a54ffefd6176203fe636cafacfaf32fd
dudu4leal <eduardoleal.ferreir4@gmail.com>
2024-10-09 09:08:32 -0300

    exercicio 2

---
+++ entendendo-metodos-java/exercicio2/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        //quadrilatero
+
+        System.out.println("exercicio quadrilatero:");
+
+        Quadrilatero.area(3);
+        Quadrilatero.area(5, 5);
+        Quadrilatero.area(7, 8, 9);
+
+    }
+}
